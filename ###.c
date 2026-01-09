9ad7d2e485767c2b9accc0e247ab85b1f552a177
SHAYAN <mohdshayan0010@gmail.com>
2026-01-09 22:46:10 +0530

    Add files via upload

---
+++ ###.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+   
+    for (int i=1;i<=4;i++)
+    {
+        for (int j=1;j<=3;j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+    getch();
+    
+}
